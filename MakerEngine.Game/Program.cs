95b51a3c16eaccf152447a068b19215eaa9cd8b2
nugget-png <nugget-png@users.noreply.github.com>
2025-02-16 19:04:51 -0500

    Create an instance of Engine in Program.cs and call Initialize() and Shutdown() functions

--- MakerEngine.Game/Program.cs
+++ MakerEngine.Game/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace MakerEngine.Game
+﻿using MakerEngine;
+
+namespace MakerEngine.Game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Engine engine = new Engine();
+            engine.Initialize();
             Console.WriteLine("Hello, World!");
+            engine.Shutdown();
         }
     }
 }
