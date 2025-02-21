a65871258e9aecfd86b8f4f32f38bb7abf6a7d89
nugget-png <nugget-png@users.noreply.github.com>
2025-02-20 20:52:54 -0500

    Attempt to create a window

--- MakerEngine.Game/Program.cs
+++ MakerEngine.Game/Program.cs
@@ -1,4 +1,7 @@
-﻿using MakerEngine.Core;
+﻿using MakerEngine;
+using MakerEngine.Core;
+using MakerEngine.Application;
+using SDL2;
 
 namespace MakerEngine.Game
 {
@@ -6,10 +9,27 @@ namespace MakerEngine.Game
     {
         static void Main(string[] args)
         {
-            Engine engine = new Engine();
-            engine.Initialize();
-            Console.WriteLine("Hello, World!");
-            engine.Shutdown();
+            try
+            {
+                Engine engine = new Engine();
+                engine.Initialize();
+
+                using (var window = new Window("Test Window", 1280, 720))
+                {
+                    window.ProcessEvents();
+                }
+
+                engine.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                SDL.SDL_ShowSimpleMessageBox(
+                    SDL.SDL_MessageBoxFlags.SDL_MESSAGEBOX_ERROR, 
+                    "Fatal Engine Error", 
+                    ex.Message, 
+                    IntPtr.Zero
+                );
+            }
         }
     }
 }
